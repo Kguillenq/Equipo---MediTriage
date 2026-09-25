1704328ee9835fd9b1364cfc42db14eea8ad345a
isi4-4 <isidrive182@gmail.com>
2026-09-25 00:48:21 -0300

    Update init.sql

--- init.sql
+++ init.sql
@@ -1,1 +1,12 @@
+-- Habilitar la extensión pgcrypto desde el arranque
 CREATE EXTENSION IF NOT EXISTS pgcrypto;
+
+-- Crear una tabla base temporal para activar la política
+CREATE TABLE IF NOT EXISTS pacientes (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    nombre VARCHAR(100)
+);
+
+-- Configurar las políticas base de Row-Level Security (RLS)
+ALTER TABLE pacientes ENABLE ROW LEVEL SECURITY;
+CREATE POLICY politicas_base_pacientes ON pacientes FOR ALL USING (false);
